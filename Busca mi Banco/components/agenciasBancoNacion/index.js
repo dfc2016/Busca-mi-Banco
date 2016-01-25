1417cc463d8e84874a4248b6cb1f2ca23ee1e239
diego corrada <diego.corrada@valmar.com.pe>
2016-01-25 21:30:09 +0000

    Added new Fx to locate Banco de la Nación agencies TODO: 1) make general the Fx to manage errors with no GPS signal 2) add icons pin and pinTarget for Banco de la Nación agencies
    Changes made by diego corrada (diego.corrada@valmar.com.pe).

---
+++ Busca mi Banco/components/agenciasBancoNacion/index.js
@@ -0,0 +1,108 @@
+'use strict';
+
+var watchIDBN = null;
+
+app.agenciasBancoNacion = kendo.observable({
+    beforeShowBN: function () {
+        console.log("DFC >>> beforeShowBN TEST Banco Nación");
+        //$(".k-map.k-marker").css("background-image", "url(images/scotiabank_28.png) !important");
+        msgWaitForMapBN();
+    },
+    onShowBN: function() {},
+    afterShowBN: function() {},
+    beforeHideBN: function () {
+        console.log("DFC >>> beforeHideBN TEST Banco Nación");
+        if (watchIDBN != null) {
+            navigator.geolocation.clearWatch(watchIDBN);
+        }
+    }
+});
+
+// START_CUSTOM_CODE_agenciasBancoNacion
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+function miPosYAgenciasBN(){
+    console.log("DFC >>> Mi Pos Y Agencias Banco Nación");
+    msgWaitForFosition("mapDondeEstoyBN");
+    watchIDBN = navigator.geolocation.watchPosition(
+        onSuccessMiPosYAgenciasBN, onErrorMiPosYAgenciasBN, {
+            timeout: 2000,
+            enableHighAccuracy: true
+        }
+    );    
+}
+
+function onSuccessMiPosYAgenciasBN(position){
+    $("#mapDondeEstoyBN").kendoMap({
+        center: [position.coords.latitude, position.coords.longitude],
+        zoom: 12,
+        layers: [
+            {
+                type: "tile",
+                urlTemplate: "http://#= subdomain #.tile.openstreetmap.org/#= zoom #/#= x #/#= y #.png",
+                subdomains: ["a", "b", "c"],
+        },
+            {
+                //Layer for positions of interesting sites
+                type: "marker",
+                dataSource: {
+                    transport: {
+                        read: {
+                            url: "http://54.213.238.161/geodata/banconacion.json",
+                            dataType: "json"
+                        }
+                    }
+                },
+                locationField: "latlng",
+                titleField: "name",
+                shape: "pin",
+        },
+            {
+                //Layer for position of location's device
+                type: "marker",
+                dataSource: dsTarget,
+                locationField: "latlng",
+                titleField: "name",
+                shape: "pinTarget",
+        },
+
+        ],
+    });
+
+    var map = $("#mapDondeEstoyBN").data("kendoMap");
+
+    var dsTarget = new kendo.data.DataSource({
+        data: [
+            {
+                latlng: [position.coords.latitude, position.coords.longitude],
+                name: "Mi posicion"
+            },
+        ]
+    });
+
+    var layerTarget = map.layers[2];
+    layerTarget.setDataSource(dsTarget);
+
+    navigator.geolocation.clearWatch(watchIDBN);
+    watchIDBN = null;
+
+    console.log("DFC >>> Mapping pin and pinTarget Banco Nación");
+}
+
+function onErrorMiPosYAgenciasBN(error){
+    // TODO: implement here the managment of GPS error in general way; parametrized...    
+}
+
+function msgWaitForMapBN(){
+    var strHTML = "<div class=\"container-fluid\">";
+    strHTML += "<div class=\"row\">";
+    strHTML += "<div class=\"col-xs-12\">";
+    strHTML += "<h3>";
+    strHTML += "<br>";
+    strHTML += "Haz click en el boton [MI POSICION Y AGENCIAS] para encontrart tu posicion y las agencias Banco de la Nación en el Mapa";
+    strHTML += "</hr>";
+    strHTML += "</div>";
+    strHTML += "</div>";
+    strHTML += "</div>";
+    $("#mapDondeEstoyBN").html(strHTML);
+}
+// END_CUSTOM_CODE_agenciasBancoNacion
